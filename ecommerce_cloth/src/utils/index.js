b5570c6fd500cb32b86c3cd31c245e1a03085c45
Alejandro Barreto <123841324+elAleDeArg@users.noreply.github.com>
2024-04-30 02:13:23 +0000

    min 40:27 button style

---
+++ ecommerce_cloth/src/utils/index.js
@@ -0,0 +1,45 @@
+export const navOptions = [
+    {
+        id: "home",
+        label: "Home",
+        path: "/",
+    },
+    {
+        id: "listing",
+        label: "All Products",
+        path: "/product/listing/all-products",
+    },
+    {
+        id: "listing-men",
+        label: "Men",
+        path: "/product/listing/men",
+    },
+    {
+        id: "listing-women",
+        label: "Women",
+        path: "/product/listing/women",
+    },
+    {
+        id: "listing-kids",
+        label: "Kids",
+        path: "/product/listing/kids",
+    },
+
+];
+
+export const adminNavOptions = [
+    {
+        id: "adminListing",
+        label: "Manage All Products",
+        path: "/admin-view/all-products",
+    },
+    {
+        id: "adminNewProduct",
+        label: "Add new Product",
+        path: "/admin-view/add-product",
+    },
+];
+
+export const styles = {
+    button: "mt-1.5 inline-block bg-black px-5 py-3 text-xs font-meddium uppercase tracking-wide text-white",
+};
